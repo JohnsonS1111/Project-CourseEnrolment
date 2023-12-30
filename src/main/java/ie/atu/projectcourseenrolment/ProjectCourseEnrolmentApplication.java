b7c32209e53961f7c7c78507f4936355d6d6c00c
Johnson Shogbaike <g00389668@atu.ie>
2023-12-30 19:42:42 +0000

    Creating course enrolment microservice

---
+++ src/main/java/ie/atu/projectcourseenrolment/ProjectCourseEnrolmentApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.projectcourseenrolment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCourseEnrolmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCourseEnrolmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/projectcourseenrolment/ProjectCourseEnrolmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.projectcourseenrolment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCourseEnrolmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
